d4d9af0be186325115172463dc0af3ebf8a06fd9
OksanaSte <36065325+OksanaSte@users.noreply.github.com>
2018-02-13 16:43:42 -0800

    assignment 1
    javascript tasks

---
+++ js/script.js
@@ -0,0 +1,128 @@
+// 1
+// var someMonth;
+// function theMonth;
+// currentMonth;
+// var summerMonth;
+// function myLibrary;
+
+// 2
+// 8.5--numeric literal expression;
+// "Zak"--string literal expression;
+// true--Boolean literal expression;
+// null--null literal expression;
+
+// 3
+//var a1 = (2+3)/500;
+//var a2 = (x-y)/10;
+
+// 4
+// var firstName;
+// var lastName;
+// var address;
+// var city;
+// var state;
+// var zipCode;
+// var yourAge;
+// var referralSource;
+// var mayWeContactYou;
+
+// 5
+// var firstName;
+// var lastName;
+// var address;
+// var firstName = "Zak";
+// var lastName = "Ruvalcaba";
+// var address = "Temecula";
+// var city = "San Diego";
+// var state = "CA";
+// var zipCode = "92108";
+// var yourAge = 33, referralSource = "Google", mayWeContactYou = false;
+
+// 6
+// var first = "Olympics" + 2018;
+// window.console.log(first);
+// var second = true + " story";
+// window.console.log(second);
+// var third = 999 + true;
+// window.console.log(third);
+
+// 7
+// window.console.log("Today is " + dayOfTheWeek);
+// var dayOfTheWeek = "Tuesday";
+
+// 8
+// var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// window.console.log(someString);
+
+// 9
+// var z = 1;
+// z = null; 
+// window.console.log(z);
+// var x;
+// window.console.log(x);
+
+// 10
+// window.console.log(typeof "Sony");
+// window.console.log(typeof -1);
+// window.console.log(typeof false);
+// window.console.log(typeof [1,2,3,4]);
+// window.console.log(typeof wtf);
+
+// 11
+// var name = "Oksana";
+// var course = "JavaScript";
+// alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+// 12
+
+// 13
+
+// 14
+
+// 15+16 
+// var name = prompt("What is your name?");
+// var course = prompt ("What class are you taking?");
+// alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+// 17
+// x = 10;
+// y = 10; 
+// z = x + y;
+// window.console.log(z);
+
+// 18
+// var x = 20;
+// x += 20;
+// window.console.log(x);
+
+// 19
+// var x = 20;
+// x *= 5;
+// window.console.log(x);
+
+// 20
+// var x = 20%3;
+// x /= 1;
+// window.console.log(x);
+
+// 21
+// var x = 25/5;
+// var y = 5/0;
+// window.console.log(x == 5 && y == Infinity);
+
+// 22
+// var x = 0;
+// var y = 0;
+// window.console.log(x > y != false);
+
+// 23
+// var widget = new Object();
+// window.console.log(typeof widget);
+
+// 24
+// var widget = new Object();
+// window.console.log(widget instanceof Object);
+
+// 25
+// var widget = new Object();
+// window.console.log(widget instanceof Number);
