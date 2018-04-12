f9a050f406dac7716c8e5678f5e7f01d909516e6
OksanaSte <36065325+OksanaSte@users.noreply.github.com>
2018-04-12 16:41:21 -0700

    assignment6
    js tasks

---
+++ Assignment6/beginning/js/strings.js
@@ -0,0 +1,49 @@
+/*eslint-env browser*/
+
+//STEP 1
+/*var name = prompt("Please enter your name.");
+alert("Your name has " + name.length + " letters.");
+
+//STEP 2
+var name = prompt("Please enter your name.");
+var number = prompt("Which letter from your name do you want to be displayed?");
+alert("The letter #" + number + " of your name is " + name.charAt(number - 1));
+
+//STEP 3
+var firstName = prompt("Please enter your first name.");
+var lastName = prompt("Please enter your last name.");
+alert("Your name is " + firstName.concat(" " + lastName));
+
+//STEP 4
+var text = "The quick brown fox jumps over the lazy dog.";
+alert(text.indexOf("fox"));
+
+//STEP 5
+var text = "The quick brown fox jumps over the lazy dog";
+alert(text.lastIndexOf("fox"));
+
+//STEP 6
+var text = "The quick brown fox jumps over the lazy dog X.";
+var name = prompt("Please enter your first and last name.");
+alert(text.replace ("X", name));
+
+
+//STEP 7
+var text = "The quick brown fox jumps over the lazy dog.";
+var word = prompt("Please enter a word.");
+alert(text.search(word));
+
+//STEP 8
+var old_string = "The quick brown fox jumps over the lazy dog.";
+var beginning = old_string.lastIndexOf("the");
+var end = old_string.indexOf("g") + 1;
+var new_string = old_string.slice(beginning, end);
+alert(new_string.toUpperCase());
+
+//STEP 9
+var string = "            THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          ";
+alert(string.trim().toLowerCase());
+
+//STEP 10
+var text = "the quick brown fox jumps over the lazy dog.";
+alert(text.charAt(0).toUpperCase() + text.slice(1));*/
